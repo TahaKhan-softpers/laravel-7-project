3139517534ead380d64635d2e03ad548ee7d79fb
TahaKhan-softpers <70566716+TahaKhan-softpers@users.noreply.github.com>
2020-09-10 13:58:43 +0500

    modified title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
 
-        <title>Laravel567</title>
+        <title>Laravel heavily modified</title>
 
 
         <!-- Fonts -->
