b8ddbb168d8e67714a25836a600de66a6104a492
TahaKhan-softpers <70566716+TahaKhan-softpers@users.noreply.github.com>
2020-09-10 15:02:47 +0500

    this is simple now

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
 
         <title>Laravel</title>
 
-        <!-- Fonts -->
+        <!-- Fonts -->g
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
         <!-- Styles -->
